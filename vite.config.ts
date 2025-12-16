0b40f9bb24cd3fb92b8babce018ea8d047d7e1b3
laurenceputterman <mlputterman@gmail.com>
2025-12-15 21:34:28 -0800

    fix github pages again

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@shared': path.resolve(__dirname, '../shared'),
+      '@shared': path.resolve(__dirname, './src/generated'),
     },
   },
   server: {
